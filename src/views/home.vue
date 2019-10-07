2bdf26b96d5893e1968bb36778a8fd34cc187572
zz632893783 <632893783@qq.com>
2019-10-07 12:59:19 +0800

    新建项目

---
+++ src/views/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <cube></cube>
+    </div>
+</template>
+<script>
+import cube from '@/components/cube/index'
+export default {
+    components: {
+        cube
+    },
+    data: function () {
+        return {
+            msg: 'home'
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped></style>
